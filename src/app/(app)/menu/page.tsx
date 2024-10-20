ad156672adea3bbfcb7c3499469042bdd1416212
henryschwerdtner <henry.schwerdtner@web.de>
2024-10-20 11:03:06 +0200

    init

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -5,7 +5,7 @@ import { WineMenuItem } from '@/components/menu/WineMenuItem';
 import config from '@payload-config'
 import { getPayloadHMR } from '@payloadcms/next/utilities'
 import { MenuType } from 'payload-types';
-
+export const dynamic = "force-dynamic";
 export default async function Menu() {
     const payload = await getPayloadHMR({
         config,
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,7 +5,7 @@ import UpComingServer from '@/components/upcoming/Server'
 import { EventRoomServer } from '@/components/event-room/Server'
 import { AboutServer } from '@/components/about/Server'
 import { ContactServer } from '@/components/contact/Server'
-
+export const dynamic = "force-dynamic";
 const Page = async () => {
   return (
     <>
