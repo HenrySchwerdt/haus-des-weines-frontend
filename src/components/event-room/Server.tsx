d5287c027eddbeecb236e31ba5474c6428a3395b
henryschwerdtner <henry.schwerdtner@web.de>
2024-11-26 16:23:39 +0100

    add images from events

--- src/components/event-room/Server.tsx
+++ src/components/event-room/Server.tsx
@@ -19,8 +19,8 @@ export async function EventRoomServer({ lang }: { lang: 'en' | 'de' }) {
         <div className="lg:px-14 px-4 md:px-6 relative bg-rouge">
             <div className="w-6 h-6 bg-rouge z-1 transform rotate-45 -translate-x-1/2 absolute left-1/2 -top-3"></div>
 
-            <div className="w-full grid grid-cols-1 md:grid-cols-3 h-full py-8 md:py-10 gap-5 md:gap-20">
-                <div className="flex flex-col justify-center items-start md:col-span-2">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 h-full py-8 md:py-10 gap-5 md:gap-20">
+                <div className="flex flex-col justify-center items-start md:col-span-1">
                     <h1 className="font-bold text-2xl md:text-4xl font-vollkorn text-white">{eventRoom.title}</h1>
                     <p className="text-white font-julius text-xl md:text-2xl mt-4">
                         {eventRoom.description}
@@ -41,7 +41,7 @@ export async function EventRoomServer({ lang }: { lang: 'en' | 'de' }) {
                     </div>
                 </div>
 
-                <div className="flex justify-center md:justify-start md:col-span-1 mt-4 md:mt-0">
+                <div>
                     <Carousel opts={{
                         align: "start",
                         loop: true,
@@ -53,7 +53,7 @@ export async function EventRoomServer({ lang }: { lang: 'en' | 'de' }) {
                                         src={(image.photo as Media).url!}
                                         fill={true} // Keep fill
                                         alt='Event Room Image'
-                                        className='rounded-md object-cover object-center'
+                                        className='rounded-md'
                                         sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw" // Ensure this is set for performance
                                         unoptimized={true}
                                     />
